318c4d67e53dbccaf424ca5f7a4aae6bd6b8833f
eric <contreras.eric18@gmail.com>
2021-08-04 14:51:04 -0400

    commit tree 6ec3ca01910575bbdea6e6c7f42bc7cbd4e5d332

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+var num1 = 2;
+var num2 = 31;
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random() * 10)+2;
+
+var num3 = 24;
+var num4 = 5;
+var mod = num3 % num4;
+
+var max = Math.max(1, 3, 18, 20);
